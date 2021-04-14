07765eda8fe24b8de28bb517f8e85e61d439fe12
jburnett <jburnett@altamodatech.com>
2021-04-14 12:14:20 -0400

    Added oh-my-zsh and z-rupa to installer

--- install-profile.sh
+++ install-profile.sh
@@ -8,7 +8,6 @@ else
         exit 1
     fi
 fi
-
 # Create symlink to dotfiles
 # TODO: might be handy to discover git repo location rather than hard coding
 ln -s $HOME/src/github.com/jburnett/profile/dotfiles $HOME/dotfiles
@@ -38,3 +37,30 @@ else
 fi
 # echo 'Create symlink for ~/.zshrc'
 ln -s $HOME/src/github.com/jburnett/profile/.zshrc $HOME/.zshrc
+
+
+# Remove existing symlink for oh-my-zsh
+if [ -L $HOME/.oh-my-zsh ]; then
+    echo "Removing .oh-my-zsh symlink"
+    rm -f $HOME/.oh-my-zsh
+else
+    if [ -e $HOME/.oh-my-zsh ]; then 
+        echo "Cannot remove $HOME/.oh-my-zsh because it is not a symlink."
+        exit 1
+    fi
+fi
+echo "Creating symlink for ~/.oh-my-zsh"
+ln -s $HOME/src/github.com/jburnett/profile/oh-my-zsh $HOME/.oh-my-zsh
+
+# Remove existing symlink for z-rupa
+if [ -L $HOME/.z-rupa ]; then
+    echo "Removing .z-rupa symlink"
+    rm -f $HOME/.z-rupa
+else
+    if [ -e $HOME/.z-rupa ]; then 
+        echo "Cannot remove $HOME/.z-rupa because it is not a symlink."
+        exit 1
+    fi
+fi
+echo "Creating symlink for ~/.z-rupa"
+ln -s $HOME/src/github.com/jburnett/profile/z-rupa $HOME/.z-rupa
