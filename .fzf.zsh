4826b7d2f4f446b5b684e029ea07d9a6cf97238d
jburnett <j.burnett@intellinet.com>
2024-05-07 10:35:20 -0400

    fzf support for Linux & MacOS diffs

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,20 +1,32 @@
-# Setup fzf
-# ---------
-## 05/30/2023, JB: use brew's fzf, not this one
-# if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-#   PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
-# fi
+# Determine if fzf on Linux
+if [[ -f /usr/share/doc/fzf/examples/completion.zsh ]]; then
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+  # Setup fzf
+  # ---------
+  ## No need to set path b/c fzf should be installed in /usr/bin
 
-# Key bindings
-# ------------
-if [[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]]; then
-    # Linux location
-    source /usr/share/doc/fzf/examples/key-bindings.zsh
-else
-    # MacOS location
-    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+  # Auto-completion
+  # ---------------
+  source /usr/share/doc/fzf/examples/completion.zsh
+
+  # Key bindings
+  # ------------
+  source /usr/share/doc/fzf/examples/key-bindings.zsh
+
+else # MacOS
+
+  # Setup fzf
+  # ---------
+  if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+    PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+  fi
+
+  # Auto-completion
+  # ---------------
+  source "/usr/local/opt/fzf/shell/completion.zsh"
+
+  # Key bindings
+  # ------------
+  source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
 fi
